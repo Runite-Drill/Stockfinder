0decf05c4ae93c396e18221e53ef5ac544ad75d2
Utopia <utopiamusicnz@gmail.com>
2022-03-12 19:04:59 -0800

    More CSS, footer, begin search results page

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,7 @@ const isLoggedIn = require('../helper/isLoggedIn');
 const router = express.Router();
 
 const searchCtrl = require("../controllers/search");
-router.get("/search", searchCtrl.search_home_get);
+router.get("/home", searchCtrl.search_home_get);
+router.get("/search", searchCtrl.search_results_get);
 
 module.exports = router;
